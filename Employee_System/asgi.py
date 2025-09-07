6354d567c24ed014edf278c7e2cac26b7e2f6f1c
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-09-07 22:27:26 +0800

    fixed asgi py

--- Employee_System/asgi.py
+++ Employee_System/asgi.py
@@ -3,6 +3,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Employee_System.settings')
 
 application = get_asgi_application()
