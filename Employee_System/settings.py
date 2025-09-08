99cd0d11c9b108e2dcfba52dd9a569aae12e3842
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-09-08 12:01:06 +0800

    added whitenoise static to installed apps

--- Employee_System/settings.py
+++ Employee_System/settings.py
@@ -173,6 +173,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static_my_project", ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media_root")
 
