2fe9d31e367f8659e5163f1beacd4a1e5cdb2309
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-09-08 16:24:54 +0800

    files for superuser

--- accounts/management/commands/create_initial_superuser.py
+++ accounts/management/commands/create_initial_superuser.py
@@ -8,16 +8,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         User = get_user_model()
-        username = os.environ.get('SUPERUSER_USERNAME')
         email = os.environ.get('SUPERUSER_EMAIL')
         password = os.environ.get('SUPERUSER_PASSWORD')
+        first_name = os.environ.get('SUPERUSER_FIRST_NAME')
+        last_name = os.environ.get('SUPERUSER_LAST_NAME')
 
-        if not all([username, email, password]):
-            self.stdout.write(self.style.ERROR('Missing SUPERUSER_USERNAME, SUPERUSER_EMAIL, or SUPERUSER_PASSWORD environment variables.'))
+        if not all([email, password, first_name, last_name]):
+            self.stdout.write(self.style.ERROR('Missing SUPERUSER_EMAIL, SUPERUSER_PASSWORD, SUPERUSER_FIRST_NAME, or SUPERUSER_LAST_NAME environment variables.'))
             return
 
         if User.objects.filter(email=email).exists():
             self.stdout.write(self.style.WARNING(f'Superuser with email "{email}" already exists. Skipping.'))
         else:
-            User.objects.create_superuser(username=username, email=email, password=password)
-            self.stdout.write(self.style.SUCCESS(f'Successfully created superuser "{username}"'))
+            # Create the superuser with the fields required by the custom Employee model
+            User.objects.create_superuser(email=email, password=password, first_name=first_name, last_name=last_name)
+            self.stdout.write(self.style.SUCCESS(f'Successfully created superuser with email "{email}"'))
