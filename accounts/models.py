3a7500a3811924ed6ddd89a560480950f23d5d7f
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-09-08 20:34:11 +0800

    bring back user but with test function delete view

--- accounts/models.py
+++ accounts/models.py
@@ -54,8 +54,7 @@ class UserManager(BaseUserManager):
 
 
 class Employee(AbstractBaseUser, PermissionsMixin):
-    # This self-referencing field was causing the deletion error and has been removed.
-    # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
