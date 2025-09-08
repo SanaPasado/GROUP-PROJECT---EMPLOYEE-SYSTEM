d63dac4c466f917036398f3b2c71410d004a8ac6
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-09-08 15:41:53 +0800

    adding superuser

---
+++ accounts/management/commands/create_initial_superuser.py
@@ -0,0 +1,24 @@
+
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    """A custom management command to create a superuser non-interactively."""
+    help = 'Creates a superuser from environment variables.'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        username = os.environ.get('SUPERUSER_USERNAME')
+        email = os.environ.get('SUPERUSER_EMAIL')
+        password = os.environ.get('SUPERUSER_PASSWORD')
+
+        if not all([username, email, password]):
+            self.stdout.write(self.style.ERROR('Missing SUPERUSER_USERNAME, SUPERUSER_EMAIL, or SUPERUSER_PASSWORD environment variables.'))
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(self.style.WARNING(f'User "{username}" already exists. Skipping.'))
+        else:
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS(f'Successfully created superuser "{username}"'))
