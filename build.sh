05faca7079d5c0056c9af28d741090918708b2d5
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-09-14 12:08:39 +0800

    removed shell

--- build.sh
+++ build.sh
@@ -9,13 +9,5 @@ python manage.py migrate
 # Create superuser if it doesn't exist
 python manage.py createsuperuser \
   --noinput \
-  --username $DJANGO_SUPERUSER_USERNAME \
-  --email $DJANGO_SUPERUSER_EMAIL || true
 
-python manage.py shell -c "from django.contrib.auth import get_user_model; \
-User = get_user_model(); \
-u, created = User.objects.get_or_create(username='$DJANGO_SUPERUSER_USERNAME'); \
-u.first_name='$DJANGO_SUPERUSER_FIRSTNAME'; \
-u.last_name='$DJANGO_SUPERUSER_LASTNAME'; \
-u.set_password('$DJANGO_SUPERUSER_PASSWORD'); \
-u.save()"
+  --email $DJANGO_SUPERUSER_EMAIL || true
