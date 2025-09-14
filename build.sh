558cf723abe831e0b7136bc3c36c8f3376f8a638
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-09-14 11:41:08 +0800

    added superuser and changed settings of render to use build.sh file

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py createsuperuser \
+  --noinput \
+  --username $DJANGO_SUPERUSER_USERNAME \
+  --email $DJANGO_SUPERUSER_EMAIL || true
