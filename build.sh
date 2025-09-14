686c0c42332e7bd1f6e21174d33d4fafb0b365c9
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-09-14 18:37:14 +0800

    added cloudinary in build sh

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 # exit on error
 set -o errexit
 
+# Diagnostic commands to check Cloudinary environment variables
+echo "--- Checking Cloudinary Vars ---"
+echo "Cloud Name Is: $CLOUDINARY_CLOUD_NAME"
+echo "API Key Is: $CLOUDINARY_API_KEY"
+echo "API Secret Is: $CLOUDINARY_API_SECRET"
+echo "--- End Check ---"
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
