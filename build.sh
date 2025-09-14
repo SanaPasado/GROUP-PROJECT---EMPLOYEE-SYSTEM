735df4450f0c6d31fb71b5788a169e69bee4005b
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-09-14 12:05:58 +0800

    added superuser creation

--- build.sh
+++ build.sh
@@ -11,3 +11,11 @@ python manage.py createsuperuser \
   --noinput \
   --username $DJANGO_SUPERUSER_USERNAME \
   --email $DJANGO_SUPERUSER_EMAIL || true
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+u, created = User.objects.get_or_create(username='$DJANGO_SUPERUSER_USERNAME'); \
+u.first_name='$DJANGO_SUPERUSER_FIRSTNAME'; \
+u.last_name='$DJANGO_SUPERUSER_LASTNAME'; \
+u.set_password('$DJANGO_SUPERUSER_PASSWORD'); \
+u.save()"
