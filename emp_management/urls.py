08278fe4b87d40273818708e5796595fd7abfe0e
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-09-16 23:14:27 +0800

    changed urls

--- emp_management/urls.py
+++ emp_management/urls.py
@@ -20,8 +20,6 @@ urlpatterns = [
 
     path('admin-panel/', admin_panel, name='admin_panel'),
 
-    path('employees/<slug:slug>/send-paycheck/', send_individual_paycheck, name='send_individual_paycheck'),
-
     path("employees/<slug:slug>/", EmpDetailView.as_view(), name="employee_detail"),
 
     path("employees/<slug:slug>/update",  EmpUpdateView.as_view(), name="employee_update"),
