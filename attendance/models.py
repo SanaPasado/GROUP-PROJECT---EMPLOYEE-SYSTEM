ea2ac95b3076f9d22c183251af08d32ef1fba4c1
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-09-19 00:03:51 +0800

    reworked logic

--- attendance/models.py
+++ attendance/models.py
@@ -2,9 +2,14 @@ from django.utils import timezone
 from django.db import models
 
 
+def get_current_date():
+    """Return current date in the default timezone"""
+    return timezone.now().date()
+
+
 class Attendance(models.Model):
     employee = models.ForeignKey('accounts.Employee', on_delete=models.CASCADE)
-    date = models.DateField(default=timezone.now)
+    date = models.DateField(default=get_current_date)
     time_in = models.DateTimeField(null=True, blank=True)
     time_out = models.DateTimeField(null=True, blank=True)
 
