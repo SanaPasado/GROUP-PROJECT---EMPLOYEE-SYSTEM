4e8854ac0fb7dc4be5b635756b00fe173165742b
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2025-10-09 23:53:39 +0800

    made timezone.now into datetime.datetime.now

--- attendance/views.py
+++ attendance/views.py
@@ -6,6 +6,7 @@ from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView
 from django.db.models import Q
 from django.utils import timezone
+import datetime
 
 from .models import Attendance
 
@@ -52,7 +53,7 @@ def record_time(request):
 
         if action == 'in':
             # Get current time only for time_in action
-            current_time = timezone.now()
+            current_time = datetime.datetime.now()
 
             # Check if already timed in today using get_or_create
             attendance, created = Attendance.objects.get_or_create(
@@ -68,7 +69,7 @@ def record_time(request):
 
         elif action == 'out':
             # Get current time only for time_out action
-            current_time = timezone.now()
+            current_time = datetime.datetime.now()
 
             try:
                 attendance = Attendance.objects.get(employee=user, date=today)
